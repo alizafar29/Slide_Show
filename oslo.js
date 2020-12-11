73156e1b76fdd097e2ded52e688ea750cde25a00
alizafar29 <74813500+alizafar29@users.noreply.github.com>
2020-12-11 10:06:42 +0530

    Update oslo.js

--- oslo.js
+++ oslo.js
@@ -1,47 +1,28 @@
-// $(function(){
-//     //configration
-//     var width = 1368;
-//     var animateSpeed = 3000;
-//     var pause1 = 3000;
-//     var currentSilde = 1;
-//     var interval;
-//     //cache DOM
-//     var $slider = $('#slider');
-//     var $slideContainer = $slider.find('.slides');
-//     var $slides = $slideContainer.find('.slide');
-
-//     function startSlider(){
-//         interval = setInterval(function(){
-//             $slideContainer.animate({'margin-left': '-='+width},animateSpeed,function(){
-//                 currentSilde++;
-//                 if(currentSilde === $slides.length){
-//                     currentSilde = 1;
-//                     $slideContainer.css('margin-left',0);
-//                 }
-//             });
-//         },pause1);
-
-//     }
-//     function pauseSlider(){
-//         clearInterval(interval);
-//     }
-//     $slider.on('mouseon',pauseSlider).on('mouseleave',startSlider);
-    
-// })
-
-
-
 $(function(){
 
-    var width = 1350,currentSlide = -2,interval,animateSpeed=1000,pause1=3000,count=0;
+    var width = 1350,currentSlide = -1,interval1 = 0,animateSpeed=1000,pause1=3000,count=0,cnt;
 
+
+    /* MouseOver And Out For Left/Right Icons. */
     $('.icon').mouseover(function(){
         console.log('mouseIn');
-        clearInterval(interval);
+        clearInterval(interval1);
     });
     $('.icon').mouseout(function(){
         console.log('mouseOut');
         startSlider();
+    });
+
+    /* MouseOver And Out For Dots. */
+    $('.dot').mouseover(function(){
+        console.log('mouseOn Dot');
+        clearInterval(interval1);
+        currentSlide = -1;
+    });
+    $('.dot').mouseout(function(){
+        console.log('mouseOut form Dot');
+        startSlider();
+
     });
 
     $(".rightIcons").click(function(){
@@ -51,22 +32,38 @@ $(function(){
         startSliderRight();
     });
 
-    $('#icon1').click(function(){
-        $(".slides").html('<img src="tech1.jpeg" alt="">');
-        $('.slides').css('margin-left',0);
+    $('#icon1').on('click',function(){
+        currentSlide = 1;
+        dotSlider();
+        //  $(".slides").html('<img src="tech1.jpeg">');
+        // $('.slides').css('margin-left',0);
+        count = 0;
+        cnt = 1;
+        dotColor();
+
 
     });
 
     $('#icon2').click(function(){
-        $(".slides").html('<img src="tech2.jpg" alt="">');
-        $('.slides').css('margin-left',0);
-       
+        currentSlide = 2;
+        dotSlider();
+        // $(".slides").html('<img src="tech2.jpg">');
+        // $('.slides').css('margin-left',0);
+        count = 0;
+        cnt = 2;
+        dotColor();
+
     });
 
     $('#icon3').click(function(){
-        $(".slides").html('<img src="tech3.jpeg" alt="">');
-        $('.slides').css('margin-left',0);
-        
+        currentSlide = 3;
+        dotSlider();
+
+        // $(".slides").html('<img src="tech3.jpeg">');
+        // $('.slides').css('margin-left',0);
+        count = 0;
+        cnt = 3;
+        dotColor();
     });
 
 
@@ -74,6 +71,7 @@ $(function(){
 function startSliderLeft(){
     if(count == 0){
         $('.slides').css('margin-left',0);
+        $('.slides').css('float','left');
     }
      $('.slides').animate({'margin-left': '-='+width},function(){
         currentSlide++;
@@ -81,7 +79,7 @@ function startSliderLeft(){
         count++;
         // console.log($('.slides').length);
         if(currentSlide === $('.slides').length){
-        currentSlide = -2;
+        currentSlide = -1;
         // console.log('Current Silde length Is : '+currentSlide);
         $('.slides').css('margin-left',0);
     }
@@ -97,7 +95,7 @@ function startSliderRight(){
         count++;
         // console.log($('.slides').length);
         if(currentSlide === $('.slides').length){
-        currentSlide = -2;
+        currentSlide = -1;
         // console.log('Current Silde length Is : '+currentSlide);
         $('.slides').css('margin-right',0);
     }
@@ -107,8 +105,38 @@ function startSliderRight(){
 
 }
 
+function dotSlider(){
+    // if(count == 0){
+    //     $('.slides').css('margin-left',0);
+    //     $('.slides').css('float','left');
+    // }
+
+    if(currentSlide == 1){
+        $('.slides').animate({'margin-left': 0},function(){
+            currentSlide = -1;
+    
+            // $('.slides').css('margin-left',0);
+        });
+    }
+    if(currentSlide == 2){
+        $('.slides').animate({'margin-left': -1350},function(){
+            currentSlide = 0;
+    
+            // $('.slides').css('margin-left',0);
+        });
+    }
+    if(currentSlide == 3){
+        $('.slides').animate({'margin-left': -2700},function(){
+            currentSlide = -1;
+    
+            // $('.slides').css('margin-left',0);
+        });
+    }
+
+    }
+
 function startSlider(){
-    interval = setInterval(function(){
+    interval1 = setInterval(function(){
         // console.log('Count Value : '+count);
         if(count == 0){
             // console.log('Executing If Loop')
@@ -118,9 +146,9 @@ function startSlider(){
         $('.slides').animate({'margin-left': '-='+width},animateSpeed,function(){
             currentSlide++;
             count++;
-            // console.log($('.slides').length);
+            console.log(currentSlide)
             if(currentSlide === $('.slides').length){
-            currentSlide = -2;
+            currentSlide = -1;
             // console.log('Current Silde length Is : '+currentSlide);
             $('.slides').css('margin-left',0);
             }
@@ -137,8 +165,10 @@ function pagination(){
     if(count == 1){
         $('#icon1').css('color','white')
         $('#icon2').css('color','red')
+        $('#icon3').css('color','white')
     }
     else if(count == 2){
+        $('#icon1').css('color','white')
         $('#icon2').css('color','white')
         $('#icon3').css('color','red')
     }
@@ -147,7 +177,29 @@ function pagination(){
         $('#icon2').css('color','white')
         $('#icon3').css('color','white')
         count = 0;
+        cnt = 0;
     }
+ }
+
+ function dotColor(){
+     console.log(cnt)
+     if(cnt == 1){
+         $('#icon1').css('color','red');
+         $('#icon2').css('color','white');
+         $('#icon3').css('color','white');
+     }
+     else if(cnt == 2){
+        $('#icon1').css('color','white');
+        $('#icon2').css('color','red');
+        $('#icon3').css('color','white');
+    }
+    else if(cnt == 3){
+        $('#icon1').css('color','white');
+        $('#icon2').css('color','white');
+        $('#icon3').css('color','red');
+        count = 1;
+    }  
+
  }
 
 });
